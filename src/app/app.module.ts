70fa343a537c2a4663eb5ecab41f400181744d36
RWEngelbrecht <rigardte@gmail.com>
2021-05-15 09:55:23 +0200

    boilerplate, contact form interface, data service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PortfolioPageComponent } from './pages/portfolio-page/portfolio-page.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PortfolioPageComponent
   ],
   imports: [
     BrowserModule,
