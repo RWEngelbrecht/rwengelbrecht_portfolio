16570d4ec9007a97f3df482e002b0510a492e925
RWEngelbrecht <rigardte@gmail.com>
2021-05-19 16:35:32 +0200

    refactored header menu

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import {MenubarModule} from 'primeng/menubar';
 import {MenuItem} from 'primeng/api';
 
 @Component({
@@ -9,6 +8,8 @@ import {MenuItem} from 'primeng/api';
 })
 export class HeaderComponent implements OnInit {
 
+  items: MenuItem[];
+
   cvDownloadLink = 'https://firebasestorage.googleapis.com/v0/b/rwengelbrecht-portfolio.appspot.com/o/rwengelbrecht_cv.pdf?alt=media&token=3ca94d62-063b-4118-b3d1-30beb7b32758';
   linkedinLink = 'https://www.linkedin.com/in/rigardt-engelbrecht-92063a11a/';
   githubLink = 'https://github.com/RWEngelbrecht';
@@ -16,6 +17,11 @@ export class HeaderComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    this.items = [
+      { label: 'CV', icon: 'pi pi-cloud-download', url: this.cvDownloadLink, style: "" },
+      { label: 'GitHub', icon: 'pi pi-github', url: this.githubLink, style: "" },
+      { label: 'LinkedIn', icon: '', url: this.linkedinLink, style: "" }
+    ]
   }
 
 }
